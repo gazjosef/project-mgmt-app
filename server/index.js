e2ce7a3ff1f1f02ce8fd1f6c9d2b13f55ee6fa6c
Gareth Hind <garethjhind@gmail.com>
2023-05-21 14:18:45 +1000

    Fixed Error

--- server/index.js
+++ server/index.js
@@ -3,9 +3,13 @@ const colors = require("colors");
 require("dotenv").config();
 const { graphqlHTTP } = require("express-graphql");
 const schema = require("./schema/schema");
+const connectDB = require("./config/db");
 const port = process.env.PORT || 5005;
 
 const app = express();
+
+// *** CONNECT TO DATABASE
+connectDB();
 
 app.use(
   "/graphql",
