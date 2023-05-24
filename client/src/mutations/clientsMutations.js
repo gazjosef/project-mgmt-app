28cc7fc706f9c985befda98a688859e2f3f4acb7
Gareth Hind <garethjhind@gmail.com>
2023-05-24 15:47:16 +1000

    Delete Client Mutation

--- client/src/mutations/clientsMutations.js
+++ client/src/mutations/clientsMutations.js
@@ -2,7 +2,7 @@ import { gql } from "@apollo/client";
 
 const DELETE_CLIENT = gql`
   mutation deleteClient($id: ID!) {
-    deleteClient(id: id) {
+    deleteClient(id: $id) {
       id
       name
       email
